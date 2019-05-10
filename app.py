f6926f5ad1df9c98e050416648dbd653494a44e6
NoharaMasato <masato338914914@gmail.com>
2019-05-10 21:16:57 +0900

    add graph

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 import time
 import json
+import time
+import datetime
 
 # 他のpcのブラウザでwebsocetを支えるためには必要らしがどこに書くかよくわからない
 #class WebSocketHandler(tornado.websocket.WebSocketHandler):  
@@ -37,13 +39,14 @@ def index():
 def pipe():
   if request.environ.get('wsgi.websocket'):
     ws = request.environ['wsgi.websocket']
-    for i in range(1,100):
+    while True:
+      t = int(time.mktime(datetime.datetime.now().timetuple()))
       svalue = ExecuteQuery('select * from accelerations order by id desc limit 1') 
-      acc_values = {'x' : str(svalue[0]),'y' : str(svalue[1]),'z' : str(svalue[2])}
+      acc_values = [{"time" : t, 'y' : str(svalue[0])},{"time" : t, 'y' : str(svalue[1])},{"time" : t, 'y' : str(svalue[2])}]
       svalue = ExecuteQuery('select * from geomagnetics order by id desc limit 1') 
-      geo_values = {'x' : str(svalue[0]),'y' : str(svalue[1]),'z' : str(svalue[2])}
+      geo_values = [{"time" : t, 'y' : str(svalue[0])},{"time" : t, 'y' : str(svalue[1])},{"time" : t, 'y' : str(svalue[2])}]
       svalue = ExecuteQuery('select * from airs order by id desc limit 1') 
-      air_values = [str(svalue[0]),str(svalue[1]),str(svalue[2]),str(svalue[3]),str(svalue[4])]
+      air_values = [{"time" : t,"y" : str(svalue[0])}, {"time" : t,"y" : str(svalue[1])}, {"time" : t, "y" : str(svalue[2])}, {"time" : t,"y" : str(svalue[3])}, {"time" : t,"y" : str(svalue[4])}]
       values = json.dumps({'acc' : acc_values,'geo' : geo_values,'air' : air_values})
       ws.send(values)
       time.sleep(3)
